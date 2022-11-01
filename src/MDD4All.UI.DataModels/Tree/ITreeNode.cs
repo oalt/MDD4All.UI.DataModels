9c6d69716d6dbf64ccfc668b48578488cf4343fa
oalt <oa@lacheweg21.de>
2022-11-01 21:25:43 +0100

    Initial code commit

---
+++ src/MDD4All.UI.DataModels/Tree/ITreeNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MDD4All.UI.DataModels.Tree
+{
+    public interface ITreeNode
+    {
+        ObservableCollection<ITreeNode> Children { get; }
+
+        ITreeNode Parent { get; set; }
+
+        int Index { get; }
+
+        bool HasChildNodes { get; }
+
+        bool IsExpanded { get; set; }
+
+        bool IsSelected { get; set; }
+
+        bool IsLoading { get; }
+
+        bool IsDisabled { get; set; }
+
+        ITreeNode SelectedNode { get; set; }
+    }
+}
