788785e53005d1299d232e535f9ad6f68c33779d
Oliver Alt <oa@lacheweg21.de>
2025-03-27 08:15:50 +0100

    New data models for Tab Control

---
+++ src/MDD4All.UI.DataModels/TabControl/ITabControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MDD4All.UI.DataModels.TabControl
+{
+    public interface ITabControl
+    {
+        ObservableCollection<ITabPage> Pages { get; set; }
+
+        bool CanClose { get; set; }
+    
+        ITabPage ActivePage { get; set; }
+
+        bool ShowTabsOnBottom { get; set; }
+    }
+}
---
+++ src/MDD4All.UI.DataModels/TabControl/ITabPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDD4All.UI.DataModels.TabControl
+{
+    public interface ITabPage
+    {
+        Type ViewType { get; }
+
+        string Header { get; }
+
+        
+    }
+}
